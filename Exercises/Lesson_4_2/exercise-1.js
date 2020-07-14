e9c034631bb1ff3e548737b145c8a1a695891b1e
Stefano Branz <stefano.branz@gmail.com>
2020-07-14 18:27:49 -0300

    Fifth exercise done

--- Exercises/Lesson_4_2/exercise-1.js
+++ Exercises/Lesson_4_2/exercise-1.js
@@ -4,6 +4,6 @@
 // Você irá utilizar esse array nos exercícios:
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (index = 0; index < 10; index += 1) {
+for (let index = 0; index < 10; index += 1) {
     console.log (numbers[index]);
 }
