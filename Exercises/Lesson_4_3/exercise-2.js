f8be5966d1f9c88700810719077736d73d768549
Stefano Branz <stefano.branz@gmail.com>
2020-07-15 19:15:11 -0300

    Changed exercise 2 statement

--- Exercises/Lesson_4_3/exercise-2.js
+++ Exercises/Lesson_4_3/exercise-2.js
@@ -1,5 +1,5 @@
-// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer,
-// seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+// 2- Para o segundo exercício, faça o mesmo que antes, 
+// mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
 // definindo valor para n
 let n = 4;
