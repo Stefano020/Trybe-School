6612e62911faf0b2df681785474c58071ec27399
Stefano Branz <stefano.branz@gmail.com>
2020-07-27 12:38:17 -0300

    Second part from exercise done

--- Exercises/Lesson_5_4/exercise1.js
+++ Exercises/Lesson_5_4/exercise1.js
@@ -1,1 +1,26 @@
-// ss
+window.onload = function(){
+  
+  let backgroundColor = document.getElementById('background-color');
+  let backgroundButton = document.getElementById('background-color-button');
+  backgroundButton.addEventListener('click', function(){
+    document.body.style.backgroundColor = `${backgroundColor.value}`;
+    let color = backgroundColor.value;
+    localStorage.setItem('backColor',color);
+  });
+  if(localStorage.backColor != undefined){
+    document.body.style.backgroundColor = localStorage.backColor;
+  }
+}
+
+  // let backgroundColor = document.getElementById('background-color');
+  // let backgroundButton = document.getElementById('background-color-button');
+  // backgroundButton.addEventListener('click', function(){
+  //   document.body.style.backgroundColor = `${backgroundColor.value}`;
+  //   let color = backgroundColor.value;
+  //   localStorage.setItem('backColor',color);
+  // });
+  // if(localStorage.backColor != undefined){
+  //   document.body.style.backgroundColor = localStorage.backColor;
+  // }
+
+  
